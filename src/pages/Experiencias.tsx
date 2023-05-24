2df652db40b6ca9bd41db76f87427cad7664d4a4
Chris Pae <clspae@uea.edu.br>
2023-05-24 14:02:36 -0400

    feat: component Experiencias

--- src/pages/Experiencias.tsx
+++ src/pages/Experiencias.tsx
@@ -1,5 +1,11 @@
+import { Carousel } from "../components";
+
 export const Experiencias = () => {
     return (
-        <section id="experiencias"></section>
-    )
-}
+        <section id="experiencia">
+            <div className="w-screen flex justify-center">
+                <Carousel />                   
+            </div>
+        </section>
+    );
+};
