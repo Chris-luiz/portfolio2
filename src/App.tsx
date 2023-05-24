75a23b6378e53f49d1ac3635ebbc2ca326dc8d3a
Chris Pae <clspae@uea.edu.br>
2023-05-24 14:03:13 -0400

    feat: pages adicionadas no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes/AppRoutes";
-import { BottomNavigation, Navbar } from "./components";
+import { BottomNavigation, Footer, Navbar } from "./components";
+import { OqueEstouEstudando } from "./pages/OqueEstouEstuando";
+import { Experiencias, Projetos } from "./pages";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <RouterProvider router={routes}></RouterProvider>
+      <Projetos />
+      <Experiencias />
+      <OqueEstouEstudando />
+      <Footer />
       <BottomNavigation />
     </div>
   );
