060cb39faeba3ec39c3c608d380328e14b93c732
Chris Pae <clspae@uea.edu.br>
2023-05-24 17:08:07 -0400

    feat: Componentes no aplicativo App

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes/AppRoutes";
 import { BottomNavigation, Footer, Navbar } from "./components";
 import { OqueEstouEstudando } from "./pages/OqueEstouEstuando";
-import { Experiencias, Projetos } from "./pages";
+import { Conhecimentos, Experiencias, Projetos } from "./pages";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <RouterProvider router={routes}></RouterProvider>
       <Projetos />
       <Experiencias />
+      <Conhecimentos />
       <OqueEstouEstudando />
       <Footer />
       <BottomNavigation />
