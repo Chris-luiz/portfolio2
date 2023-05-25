88bf1c87c6dac41552a37b1a12bdf360f97d0a27
Chris Pae <clspae@uea.edu.br>
2023-05-25 15:57:23 -0400

    fix: path das imagens

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -15,7 +15,7 @@ const experiencias: ICarousel[] = [
     {
         cargo: 'Estagiario em Desenvolvimento',
         nomeEmpresa: "Secretaria Municipal de Educação de Manaus (Semed-AM)",
-        img: "semed.jpg",
+        img: "jobs_images/semed.jpg",
         periodo: "01/2022 - 06/2022",
         tipo: "estagio",
         responsabilidades: "Participei dos projetos de desenvolvimento quesão solicitados ao DGTI; Correção de Bugs e sugestão de melhorias dosprojetos mantidos pela Semed; Solução de tarefas atribuídas; Estudo e aprendizadocontinuo.",
@@ -24,7 +24,7 @@ const experiencias: ICarousel[] = [
     {
         cargo: 'Estagiario em T.I',
         nomeEmpresa: "Superintendencia da Zona Franca de Manaus (SUFRAMA)",
-        img: "suframa.jpg",
+        img: "jobs_images/suframa.jpg",
         periodo: "01/2022 - 06/2022",
         tipo: "estagio",
         responsabilidades: "Implementação melhorias constantes dos Bancosde Dados implementando Triggers e realizando relatórios através de consultas; Criação deDashboards no Power BI realizando a análise e tratamento de dados.",
@@ -33,7 +33,7 @@ const experiencias: ICarousel[] = [
     {
         cargo: 'Analista de Sistemas Jr.',
         nomeEmpresa: "Universidade do Estado do Amazonas",
-        img: "uea.jpg",
+        img: "jobs_images/uea.jpg",
         periodo: "06/2022 - 06/2023",
         tipo: "CLT",
         responsabilidades: "Fui responsavel pela elaboração e criação do Front-End das aplicações RU-Admin e RU-Caixa; Elaborei a construção do Front-End para se comunicar e consumir API's da UEA; Participei de reuniões e levantamento de requisitos e regra de negocio",
