837c3157143c5f04c386f2fd22925ce2e3a56041
Chris Pae <clspae@uea.edu.br>
2023-05-26 16:46:54 -0400

    refactor: espacamento e estilizacao

--- src/pages/Conhecimentos.tsx
+++ src/pages/Conhecimentos.tsx
@@ -4,10 +4,9 @@ import { CONHECIMENTOS } from "../constants";
 export const Conhecimentos = () => {
     return (
         <section id='conhecimentos' className="bg-white dark:bg-gray-900">
-            <div className="container flex flex-col gap-10 px-6 py-12 mx-auto justify-center">
-                <div className="border-l-4 pl-2 border-blue-400 rounded-sm"> 
-
-                    <h1 className="mt-2 text-2xl font-semibold text-blue-800 md:text-3xl dark:text-white">Conhecimentos</h1>
+            <div className="border-l-4 pl-4 border-blue-400 rounded-sm container flex flex-col gap-10 px-6 mx-auto justify-center">
+                <div> 
+                    <h1 className="mt-2 text-2xl font-mono text-blue-800 md:text-3xl dark:text-white">Conhecimentos</h1>
                     <p className="font-medium text-blue-500 dark:text-blue-400">ferramentas e linguagens</p>
 
                     <p className="mt-3 text-gray-500 dark:text-gray-400">Essas são algumas linguagens e ferramentas que conheço e utilizo.</p>
