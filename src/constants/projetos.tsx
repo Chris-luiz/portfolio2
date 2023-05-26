22447cda9cc9ec22cae4d3cd8fef987dad4104a2
Chris Pae <clspae@uea.edu.br>
2023-05-26 16:15:22 -0400

    feat: const projetos

---
+++ src/constants/projetos.tsx
@@ -0,0 +1,13 @@
+import { ICardProps } from "../components";
+
+export const PROJETOS: ICardProps[] = [
+    {
+        nome: "react-challenge-dots-at-screen",
+        img: "projects_images/dots-at-screen.png",
+        description: "Projeto front-end usado em entrevista que encontrei no canal Front-End Begginer. Consiste em criar um circulo no local clicado pelo mouse. Link: https://www.youtube.com/watch?v=dOsBtM2U018",
+        url: "https://chris-luiz.github.io/click-and-enjoy/",
+        githubLink: "https://github.com/Chris-luiz/click-and-enjoy",
+        langUser: ["React, ", "Typescript"],
+    },
+    
+]
