5a971784747fe1f9bea40e2f949483e46d665364
Chris Pae <clspae@uea.edu.br>
2023-05-26 16:57:19 -0400

    feat: const experiencias

---
+++ src/constants/experiencias.tsx
@@ -0,0 +1,31 @@
+import { TCarousel } from "../types";
+
+export const EXPERIENCIAS: TCarousel[] = [
+    {
+        cargo: 'Estagiario em Desenvolvimento',
+        nomeEmpresa: "Secretaria Municipal de Educação de Manaus (Semed-AM)",
+        img: "jobs_images/semed.jpg",
+        periodo: "01/2022 - 06/2022",
+        tipo: "estagio",
+        responsabilidades: "Participei dos projetos de desenvolvimento quesão solicitados ao DGTI; Correção de Bugs e sugestão de melhorias dosprojetos mantidos pela Semed; Solução de tarefas atribuídas; Estudo e aprendizadocontinuo.",
+        tecnologiasUtilizadas: ["HTML", "CSS", "JavaScript", "PHP", "Yii2 framework", "Banco de Dados Oracle SQL", "GIT"]
+    },
+    {
+        cargo: 'Estagiario em T.I',
+        nomeEmpresa: "Superintendencia da Zona Franca de Manaus (SUFRAMA)",
+        img: "jobs_images/suframa.jpg",
+        periodo: "01/2022 - 06/2022",
+        tipo: "estagio",
+        responsabilidades: "Implementação melhorias constantes dos Bancosde Dados implementando Triggers e realizando relatórios através de consultas; Criação deDashboards no Power BI realizando a análise e tratamento de dados.",
+        tecnologiasUtilizadas: ["Sistema Open-Source Redmine", "MySQL", "PHPMyAdmin", "Power BI"]
+    },
+    {
+        cargo: 'Analista de Sistemas Jr.',
+        nomeEmpresa: "Universidade do Estado do Amazonas",
+        img: "jobs_images/uea.jpg",
+        periodo: "06/2022 - 06/2023",
+        tipo: "CLT",
+        responsabilidades: "Fui responsavel pela elaboração e criação do Front-End das aplicações RU-Admin e RU-Caixa; Elaborei a construção do Front-End para se comunicar e consumir API's da UEA; Participei de reuniões e levantamento de requisitos e regra de negocio",
+        tecnologiasUtilizadas: ["React", "Typescript", "Cypress", "Git", "GitLab", "Docker"]
+    },
+]
