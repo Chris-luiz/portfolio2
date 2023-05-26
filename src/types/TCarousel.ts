dbac94b1d585246894a877d2e2c6449028e440e8
Chris Pae <clspae@uea.edu.br>
2023-05-26 16:55:49 -0400

    feat: type TCarousel

---
+++ src/types/TCarousel.ts
@@ -0,0 +1,9 @@
+export type TCarousel = {
+    nomeEmpresa: string;
+    cargo: string;
+    img: string;
+    periodo: string;
+    tipo: 'estagio' | 'CLT'
+    responsabilidades: string;
+    tecnologiasUtilizadas: string[];
+}
