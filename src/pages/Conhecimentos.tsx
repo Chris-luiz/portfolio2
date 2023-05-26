5943155f912a2e4c0040f3d15589d2695cd3e131
Chris Pae <clspae@uea.edu.br>
2023-05-26 16:14:58 -0400

    feat: componente Conhecimento

--- src/pages/Conhecimentos.tsx
+++ src/pages/Conhecimentos.tsx
@@ -1,7 +1,28 @@
+import { ToolsKnowledge } from "../components/ToolsKnowledge";
+import { CONHECIMENTOS } from "../constants";
+
 export const Conhecimentos = () => {
     return (
-        <section id='conhecimentos'>
-            
+        <section id='conhecimentos' className="bg-white dark:bg-gray-900">
+            <div className="container flex flex-col gap-10 px-6 py-12 mx-auto justify-center">
+                <div className="border-l-4 pl-2 border-blue-400 rounded-sm"> 
+
+                    <h1 className="mt-2 text-2xl font-semibold text-blue-800 md:text-3xl dark:text-white">Conhecimentos</h1>
+                    <p className="font-medium text-blue-500 dark:text-blue-400">ferramentas e linguagens</p>
+
+                    <p className="mt-3 text-gray-500 dark:text-gray-400">Essas são algumas linguagens e ferramentas que conheço e utilizo.</p>
+                </div>
+
+                <div className="shadow-sm rounded-sm grid grid-cols-1 gap-12 lg:col-span-2 sm:grid-cols-3">
+                    {CONHECIMENTOS.map(conhecimento => (
+                        <ToolsKnowledge
+                            icon={conhecimento.icon}
+                            lang={conhecimento.lang}
+                            text={conhecimento.text}
+                        />
+                    ))}
+                </div>
+            </div>
         </section>
     );
 }
