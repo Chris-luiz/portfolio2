818aa59d86aef39e5fa2a934b4b076a558bb1b3b
Chris Pae <clspae@uea.edu.br>
2023-05-26 16:57:28 -0400

    refactor: correcao de importacao

--- src/pages/Projetos.tsx
+++ src/pages/Projetos.tsx
@@ -4,19 +4,21 @@ import { PROJETOS } from "../constants/projetos"
 export const Projetos = () => {
     return (
         <section id="projetos">
-            <h1 className="text-2xl text-gray-700 text-center">Projetos</h1>
-            <div className="flex gap-4 flex-wrap justify-center">
-                {PROJETOS.map(project => (
-                    <Card
-                        key={project.nome}
-                        nome={project.nome}
-                        img={project.img}
-                        description={project.description}
-                        githubLink={project.githubLink}
-                        langUser={project.langUser}
-                        url={project.url}
-                    />
-                ))}
+            <div className="flex justify-center flex-col px-12 py-12">
+                <h1 className="text-2xl text-gray-700 text-center m-20">Projetos</h1>
+                <div className="flex gap-4 flex-wrap justify-center">
+                    {PROJETOS.map(project => (
+                        <Card
+                            key={project.nome}
+                            nome={project.nome}
+                            img={project.img}
+                            description={project.description}
+                            githubLink={project.githubLink}
+                            langUser={project.langUser}
+                            url={project.url}
+                        />
+                    ))}
+                </div>
             </div>
         </section>
     )
