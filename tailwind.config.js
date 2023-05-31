c3f3df39d89bf1220acb733d26b04e80a77007aa
Chris-luiz <christian.luiz.de.sousa@gmail.com>
2023-05-30 22:31:58 -0400

    refactor: removido xs do tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
-
 
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    screens: {
-      'xs': '350px',
-      ...defaultTheme.screens
-    },
     extend: {},
   },
   plugins: [],
