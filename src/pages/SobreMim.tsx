f7e47210fa25619b811f0ecf60453cdfd0c252b3
Chris-luiz <christian.luiz.de.sousa@gmail.com>
2023-05-30 22:31:12 -0400

    feat: dark-mode, responsividade e componentizacao

--- src/pages/SobreMim.tsx
+++ src/pages/SobreMim.tsx
@@ -1,34 +1,23 @@
-import { BsGit } from 'react-icons/bs';
-import { SiTypescript, SiJavascript, } from 'react-icons/si'
-import { FaReact, FaCss3Alt, FaHtml5 } from 'react-icons/fa'
+import { Title } from '../components';
+import { PrincipaisTecnologias } from '../components/sobreMim';
 
 export const SobreMim = () => {
     return (
         <main id="sobre-mim">
-            <div className='w-screen flex justify-center items-center md:mt-28 mt-20'>
-                <div className="w-2/3 md:grid grid-cols-4 border shadow-lg md:p-10 xs:p-2 rounded-lg gap-4 ">
-                    <div className='col-span-1'>
-                        <figure>
-                            <img src="profile.jpg" alt="" className='w-56 h-72 object-cover rounded-lg' />
-                        </figure>
-                    </div>
+            <div className='w-screen flex justify-center py-12'>
+                <div className="dark:bg-gray-700 sm:w-2/4 container p-4 shadow rounded-lg flex flex-col items-center sm:flex-row gap-4 border dark:border-blue-400">
 
-                    <div className='flex flex-col col-span-3'>
-                        <div>
-                            <h1 className='text-2xl text-blue-900'>Christian Luiz de Souza Paes</h1>
-                            <p className='text-1xl font-mono text-blue-900'>Desenvolvedor Front-End Jr.</p>
-                            <br />
-                            <p className='font-light'>Tenho 21 anos e sou um desenvolvedor Front-End apaixonado por transformar ideias em realidade. Atualmente minha principal stack se concentra no ecossistema JS, com Node, TypeScript e suas bibliotecas mais famosas (Yup, Axios, React, etc). Curso <strong className='font-bold'>Sistemas de Informação</strong> pela Fametro.</p>
-                        </div>
+                    <img src="profile.jpg" alt="foto de perfil" className='w-72 h-72 object-cover rounded-lg' />
+
+                    <div className='flex flex-col p-2'>
+                        <Title 
+                          title='Christian Luiz de Souza Paes' 
+                          subTitle='Desenvolvedor Front-End Jr.'
+                        />
+                        <br />
+                        <p className='dark:text-white'>Tenho 21 anos e sou um desenvolvedor Front-End apaixonado por transformar ideias em realidade. Atualmente minha principal stack se concentra no ecossistema JS, com Node, TypeScript e suas bibliotecas mais famosas (Yup, Axios, React, etc). Curso <strong className='font-bold'>Sistemas de Informação</strong> pela Fametro.</p>
                         <div className='border-t-2 my-5'></div>
-                        <div className='flex gap-10 justify-around '>
-                            <FaHtml5 className='text-gray-800 w-6 h-6 sm:w-10 sm:h-10 hover:fill-orange-600' />
-                            <FaCss3Alt className='text-gray-800 w-6 h-6 sm:w-10 sm:h-10 hover:fill-blue-600' />
-                            <SiJavascript className='text-gray-800 w-6 h-6 sm:w-10 sm:h-10 hover:fill-yellow-400' />
-                            <SiTypescript className='text-gray-800 w-6 h-6 sm:w-10 sm:h-10 hover:fill-blue-600' />
-                            <FaReact className='text-gray-800 w-6 h-6 sm:w-10 sm:h-10 hover:fill-cyan-500' />
-                            <BsGit className='text-gray-800 w-6 h-6 sm:w-10 sm:h-10 hover:fill-red-600' />
-                        </div>
+                        <PrincipaisTecnologias />
                     </div>
                 </div>
             </div>
