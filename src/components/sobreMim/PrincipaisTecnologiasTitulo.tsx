b173b55283ca4b83a1313641738b8d39a84516e6
Chris-luiz <christian.luiz.de.sousa@gmail.com>
2023-05-30 22:28:55 -0400

    feat: componente PrincipaisTecnologiasTitulo

---
+++ src/components/sobreMim/PrincipaisTecnologiasTitulo.tsx
@@ -0,0 +1,9 @@
+
+export const PrincipaisTecnologiasTitulos = () => {
+  return (
+    <>
+      <p className='dark:text-white text-2xl  text-blue-900'>Principais Tecnologias</p>
+      <p className='dark:text-gray-400 text-1xl text-blue-500 mb-4'>Que estão mais no meu dia-a-dia</p>
+    </>
+  )
+}
