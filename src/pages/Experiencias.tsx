8ea24273fdd294b17b2ebaa1046a6d0aa06a290c
Chris-luiz <christian.luiz.de.sousa@gmail.com>
2023-05-30 22:30:24 -0400

    feat: padding OqueEstouEstudando

--- src/pages/Experiencias.tsx
+++ src/pages/Experiencias.tsx
@@ -3,7 +3,7 @@ import { Carousel } from "../components";
 export const Experiencias = () => {
     return (
         <section id="experiencia">
-            <div className="w-screen flex justify-center">
+            <div className="w-screen flex justify-center py-12">
                 <Carousel />                   
             </div>
         </section>
