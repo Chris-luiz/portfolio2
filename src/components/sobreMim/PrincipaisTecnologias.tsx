102435dda06bebec3e454aebb826eaa88f081292
Chris-luiz <christian.luiz.de.sousa@gmail.com>
2023-05-30 22:28:29 -0400

    feat: componente PrincipaisTecnologias

---
+++ src/components/sobreMim/PrincipaisTecnologias.tsx
@@ -0,0 +1,11 @@
+import { PrincipaisTecnologiasIcones } from './PrincipaisTecnologiasIcones';
+import { PrincipaisTecnologiasTitulos } from './PrincipaisTecnologiasTitulo';
+
+export const PrincipaisTecnologias = () => {
+  return (
+    <>
+      <PrincipaisTecnologiasTitulos />
+      <PrincipaisTecnologiasIcones />
+    </>
+  )
+}
