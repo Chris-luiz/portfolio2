0fae33a4d31f6dae38e24b7608dbc7d68663ab95
Chris-luiz <christian.luiz.de.sousa@gmail.com>
2023-05-30 22:30:57 -0400

    feat: componentizacao, estilzacao e dark-mode conh

--- src/pages/Conhecimentos.tsx
+++ src/pages/Conhecimentos.tsx
@@ -1,18 +1,20 @@
+import { Title } from "../components";
 import { ToolsKnowledge } from "../components/ToolsKnowledge";
 import { CONHECIMENTOS } from "../constants";
 
 export const Conhecimentos = () => {
     return (
-        <section id='conhecimentos' className="bg-white dark:bg-gray-900">
-            <div className="border-l-4 pl-4 border-blue-400 rounded-sm container flex flex-col gap-10 px-6 mx-auto justify-center">
-                <div> 
-                    <h1 className="mt-2 text-2xl font-mono text-blue-800 md:text-3xl dark:text-white">Conhecimentos</h1>
-                    <p className="font-medium text-blue-500 dark:text-blue-400">ferramentas e linguagens</p>
+        <section id='conhecimentos'>
+            <div className="bg-white dark:bg-gray-900 border-l-4 pl-4 border-blue-400 rounded-sm container flex flex-col gap-10 px-6 py-12 mx-auto justify-center">
 
-                    <p className="mt-3 text-gray-500 dark:text-gray-400">Essas são algumas linguagens e ferramentas que conheço e utilizo.</p>
-                </div>
+                <Title 
+                  title="Conhecimentos"
+                  subTitle="ferramentas e linguagens"
+                  legend="Essas são algumas linguagens e ferramentas que conheço e utilizo."
+                
+                />
 
-                <div className="shadow-sm rounded-sm grid grid-cols-1 gap-12 lg:col-span-2 sm:grid-cols-3">
+                <div className="grid grid-cols-1 gap-12 lg:col-span-2 sm:grid-cols-3">
                     {CONHECIMENTOS.map(conhecimento => (
                         <ToolsKnowledge
                             icon={conhecimento.icon}
