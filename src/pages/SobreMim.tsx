bec78262e1a82fd7469932c254682e385ef822bc
Chris Pae <clspae@uea.edu.br>
2023-06-28 14:07:52 -0400

    foto de perfil

--- src/pages/SobreMim.tsx
+++ src/pages/SobreMim.tsx
@@ -7,7 +7,7 @@ export const SobreMim = () => {
             <div className='w-screen flex justify-center md:my-4'>
                 <div className="dark:bg-gray-700 sm:w-auto lg:w-3/4 container p-4 shadow rounded-lg flex flex-col items-center sm:flex-row gap-4 border dark:border-blue-400">
 
-                    <img src="profile.jpg" alt="foto de perfil" className='w-72 h-72 object-cover rounded-lg' />
+                    <img src="profile2.jpg" alt="foto de perfil" className='w-72 h-72 object-cover rounded-lg' />
 
                     <div className='flex flex-col'>
                         <Title title='Christian Luiz de Souza Paes' subTitle='Desenvolvedor Front-End Jr.' />
